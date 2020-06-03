c11e6ed99bece067448a9f81e368b4bd23cc7e49
TheMagicalAubster <aubrey.blairpattison@gmail.com>
2020-06-03 22:24:50 +0000

    user input handling

--- play.js
+++ play.js
@@ -1,5 +1,22 @@
 //play.js
 const { connect } = require('./client');
-
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function(key) {
+    if (key === '\u0003') {
+        process.exit();
+      }
+  }
+
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    
+    return stdin;
+  }
+
+  const stdin = setupInput();
+  stdin.on('data', handleUserInput);
